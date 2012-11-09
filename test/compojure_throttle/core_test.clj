be2e260f3e74ad9389e2d1ff4196a7f559e3b104
Ben Griffiths <benjamin.griffiths@nokia.com>
2012-11-09 16:12:36 +0000

    Improve test syntax

--- test/compojure_throttle/core_test.clj
+++ test/compojure_throttle/core_test.clj
@@ -11,11 +11,17 @@
       (ok-or-throttle {:remote-addr "10.0.0.1"}) => (contains {:status 200}))
 
 (fact "Multiple calls do get throttled"
-      (doseq [x (range 3)]
+      (dotimes [x 3]
         (ok-or-throttle {:remote-addr "10.0.0.2"}) => (contains {:status 200}))
       (ok-or-throttle {:remote-addr "10.0.0.2"}) => (contains {:status 420}))
 
 (fact "The bucket refills"
-      (doseq [x (range 10)]
-        (ok-or-throttle {:remote-addr "10.0.0.2"}) => (contains {:status 200})
+      (dotimes [x 10]
+        (ok-or-throttle {:remote-addr "10.0.0.3"}) => (contains {:status 200})
         (Thread/sleep 400)))
+
+(fact "Calls get throttled for custom tokens"
+      (let [handler (throttle (fn [req] {:status 200}) (fn [req] (:user req)))]
+        (dotimes [x 3]
+            (ok-or-throttle {:user "token-blah"}) => (contains {:status 200})))
+      (ok-or-throttle {:user "token-blah"}) => (contains {:status 420}))
