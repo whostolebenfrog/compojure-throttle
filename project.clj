5cfdee2a2852daec1a06da3d821385bc2c930720
Ben Griffiths <benjamin.griffiths@nokia.com>
2012-11-09 11:29:19 +0000

    Add lein project flie

---
+++ project.clj
@@ -0,0 +1,14 @@
+(defproject compojure-throttle "0.1.0-SNAPSHOT"
+
+  :description "Throttling middleware for compojure"
+
+  :url "http://example.com/FIXME"
+
+  :license {:name "Eclipse Public License"
+            :url "http://www.eclipse.org/legal/epl-v10.html"}
+            
+  :dependencies [[org.clojure/clojure "1.4.0"]
+                 [org.clojure/core.cache "0.6.2"]
+                 [environ "0.3.0"]]
+
+  :profiles {:dev {:plugins [[lein-midje "2.0.0-SNAPSHOT"]]}})
