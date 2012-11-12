6cf6b7fb598bf899b03023bc0388b9dde5d0a825
Ben Griffiths <benjamin.griffiths@nokia.com>
2012-11-12 12:35:43 +0000

    Prefix props with service

--- src/compojure_throttle/core.clj
+++ src/compojure_throttle/core.clj
@@ -5,16 +5,16 @@
             [clj-time.core  :as core-time]))
 
 (def ^:private defaults
-  {:compojure-throttle-enabled "true"
-   :compojure-throttle-ttl    1000
-   :compojure-throttle-tokens 3
-   :compojure-throttle-response-code 420})
+  {:service-compojure-throttle-enabled "true"
+   :service-compojure-throttle-ttl    1000
+   :service-compojure-throttle-tokens 3
+   :service-compojure-throttle-response-code 420})
 
 (defn enabled?
   []
   (boolean (Boolean/valueOf
-            (or (env :compojure-throttle-enabled)
-                (defaults :compojure-throttle-enabled)))))
+            (or (env :service-compojure-throttle-enabled)
+                (defaults :service-compojure-throttle-enabled)))))
 
 (defn- prop
   [key]
@@ -22,7 +22,7 @@
                 (defaults key))))
 
 (def ^:private requests
-  (atom (cache/ttl-cache-factory {} :ttl (prop :compojure-throttle-ttl))))
+  (atom (cache/ttl-cache-factory {} :ttl (prop :service-compojure-throttle-ttl))))
 
 (defn- update-cache
   [id tokens]
@@ -30,8 +30,8 @@
 
 (defn- token-period
   []
-  (/ (prop :compojure-throttle-ttl)
-     (prop :compojure-throttle-tokens)))
+  (/ (prop :service-compojure-throttle-ttl)
+     (prop :service-compojure-throttle-tokens)))
 
 (defn- record
   [tokens]
@@ -41,7 +41,7 @@
 (defn- throttle?
   [id]
   (when-not (cache/has? @requests id)
-    (update-cache id (record (prop :compojure-throttle-tokens))))
+    (update-cache id (record (prop :service-compojure-throttle-tokens))))
   (let [entry (cache/lookup @requests id)
         spares (/ (core-time/in-msecs (core-time/interval
                                        (:datetime entry)
@@ -66,7 +66,7 @@ the request as its single argument"
    (fn [req]
      (if (and (enabled?)
               (throttle? (finder req)))
-       {:status (prop :compojure-throttle-response-code)
+       {:status (prop :service-compojure-throttle-response-code)
         :body "You have sent too many requests. Please wait before retrying."}
        (handler req))))
 ([handler]
